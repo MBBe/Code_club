13d21abbf4b0890098ba2d8049d6d39b540e025d
ManassehBBTNA <manasseh.boyd@nationalarchives.gsi.gov.uk>
2018-01-09 10:56:32 +0000

    Fizz Buzz testing stuff....

---
+++ CodeClub-Testing/Multiples.js
@@ -0,0 +1,16 @@
+class Multiples {
+    // Here code needs to be written to assign the Multiples of three to Print Fizz
+    //and the multiples of 5 to print Buzz
+
+    //maybe
+     //set_numbers() {
+     //this.Multiples_of_3 = this.set_numbers (3, "Fizz")
+     //this.Multiples_of_5 = this.set_numbers (5, "Buzz")
+     //}
+
+      //get full_numbers (){
+      //return this.Multiples_of_3 +
+      //return this.Multiples_of_5 +
+      //}
+}
+module.exports = Multiples;
---
+++ CodeClub-Testing/Test/test.js
@@ -0,0 +1,51 @@
+//var assert = require ('assert');
+//var numbers = require('../Numbers');
+//var Multiples = require ('../Multiples');
+
+
+
+//The following test would have a description of a failed test.
+//It would have a description of how the multiples of three Integers
+// convert to the string "Fizz" and the same for 5
+
+
+/*describe('Multiples', function () {
+    describe('the constructor', function () {
+
+        it('should throw if Multiple of 3 is not passed as Fizz', function () {
+            assert.throws(() => new Multiples('bad'));
+        });
+
+    });
+
+    describe('Multiples of 3', function () {
+
+        let conversions = [
+            {int: 3, str: "Fizz"},
+            {int: 6, str: "Fizz"},
+            //really this could be a loop ...?
+        ];
+
+        conversions.forEach(function (i) {
+            it(`should be "${i.str}" when passed ${i.int}`, function () {
+                let k = new Multiples(i.int);
+                assert.equal(k.Multiples_of_3, i.str);
+            });
+        });
+
+
+    });
+
+    describe('The full numbers', function () {
+
+
+        numbers.forEach(function (i) {
+            it(`should be "${i.str}" when passed ${i.int}`, function () {
+                let k = new Roman(i.int);
+                assert.equal(k.full_numbers, i.str);
+            });
+        });
+    })
+
+});  */
+
